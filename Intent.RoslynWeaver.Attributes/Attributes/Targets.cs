fa83c1323436d97ff8547687feb8e07694427dbe
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-09-04 14:00:08 +0200

    Add `EventField` to `Targets`.
    Changed enum values to be hexadecimal for easier incrementation in the future.

--- Intent.RoslynWeaver.Attributes/Attributes/Targets.cs
+++ Intent.RoslynWeaver.Attributes/Attributes/Targets.cs
@@ -5,21 +5,22 @@ namespace Intent.RoslynWeaver.Attributes
     [Flags]
     public enum Targets
     {
-        Classes = 1,
-        Methods = 2,
-        Constructors = 4,
-        Fields = 8,
-        Properties = 16,
-        Interfaces = 32,
-        Enums = 64,
-        Namespaces = 128,
-        Usings = 256,
-        Operators = 512,
-        OperatorConversions = 1024,
-        Structs = 2048,
-        Records = 4096,
-        EnumMembers = 8192,
-        Delegates = 16384,
-        TopLevelStatements = 32768
+        Classes = 0x0000_0001,
+        Methods = 0x0000_0002,
+        Constructors = 0x0000_0004,
+        Fields = 0x0000_0008,
+        Properties = 0x0000_0010,
+        Interfaces = 0x0000_0020,
+        Enums = 0x0000_0040,
+        Namespaces = 0x0000_0080,
+        Usings = 0x0000_0100,
+        Operators = 0x0000_0200,
+        OperatorConversions = 0x0000_0400,
+        Structs = 0x0000_0800,
+        Records = 0x0000_1000,
+        EnumMembers = 0x0000_2000,
+        Delegates = 0x0000_4000,
+        TopLevelStatements = 0x0000_8000,
+        EventField = 0x0001_0000
     }
 }
