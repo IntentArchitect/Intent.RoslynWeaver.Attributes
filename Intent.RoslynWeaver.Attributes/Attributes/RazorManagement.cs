c932f517b05e2b279ee9dd0d8e74d9c44067ea5c
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2024-06-09 20:42:51 +0200

    Add Razor Weaving instructions

---
+++ Intent.RoslynWeaver.Attributes/Attributes/RazorManagement.cs
@@ -0,0 +1,15 @@
+﻿namespace Intent.RoslynWeaver.Attributes
+{
+    public static class Intent
+    {
+        public static string Fully = nameof(Fully);
+        public static string FullyBody = nameof(FullyBody);
+        public static string FullySignature = nameof(FullySignature);
+        public static string Ignore = nameof(Ignore);
+        public static string IgnoreBody = nameof(IgnoreBody);
+        public static string IgnoreSignature = nameof(IgnoreSignature);
+        public static string Merge = nameof(Merge);
+        public static string MergeBody = nameof(MergeBody);
+        public static string MergeSignature = nameof(MergeSignature);
+    }
+}
