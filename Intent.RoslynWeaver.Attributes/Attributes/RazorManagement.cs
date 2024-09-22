e8201957a081ed0f26456f2054f2236969bbb1b8
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2024-09-22 12:58:59 +0200

    Change value to be empty string as was rendering when used in certain places in Razor files.

--- Intent.RoslynWeaver.Attributes/Attributes/RazorManagement.cs
+++ Intent.RoslynWeaver.Attributes/Attributes/RazorManagement.cs
@@ -2,14 +2,14 @@
 {
     public static class Intent
     {
-        public static string Fully = nameof(Fully);
-        public static string FullyBody = nameof(FullyBody);
-        public static string FullySignature = nameof(FullySignature);
-        public static string Ignore = nameof(Ignore);
-        public static string IgnoreBody = nameof(IgnoreBody);
-        public static string IgnoreSignature = nameof(IgnoreSignature);
-        public static string Merge = nameof(Merge);
-        public static string MergeBody = nameof(MergeBody);
-        public static string MergeSignature = nameof(MergeSignature);
+        public static string Fully = string.Empty;
+        public static string FullyBody = string.Empty;
+        public static string FullySignature = string.Empty;
+        public static string Ignore = string.Empty;
+        public static string IgnoreBody = string.Empty;
+        public static string IgnoreSignature = string.Empty;
+        public static string Merge = string.Empty;
+        public static string MergeBody = string.Empty;
+        public static string MergeSignature = string.Empty;
     }
 }
