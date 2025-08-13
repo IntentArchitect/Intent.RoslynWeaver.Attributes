2823581c9e11027b0165d3ef2c969295339650fe
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2025-08-13 15:47:38 +0200

    Update Razor instructions based on team feedback and bump major version.

--- Intent.RoslynWeaver.Attributes/Attributes/RazorManagement.cs
+++ Intent.RoslynWeaver.Attributes/Attributes/RazorManagement.cs
@@ -4,35 +4,144 @@ namespace Intent.RoslynWeaver.Attributes
 {
     public static class Intent
     {
-        public static string Fully = string.Empty;
-        public static string FullyBody = string.Empty;
-        public static string FullySignature = string.Empty;
-        public static string Ignore = string.Empty;
-        public static string IgnoreBody = string.Empty;
-        public static string IgnoreSignature = string.Empty;
-        public static string Merge = string.Empty;
-        public static string MergeBody = string.Empty;
-        public static string MergeSignature = string.Empty;
+        /// <summary>
+        /// This element must be fully managed by the code weaver and any user changes should
+        /// be removed.
+        /// </summary>
+        /// <param name="path">
+        /// Optional. Move the element's location from where it was in the generated content based
+        /// on the provided <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see>.
+        /// </param>
+        public static string Fully(string path = null) => string.Empty;
 
         /// <summary>
-        /// This element is matched against the element at the specified XPath-like <paramref name="path"/>,
-        /// see the <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">documentation</see> for
-        /// further details.
+        /// The <paramref name="attributes">specified</paramref> attributes of this element must
+        /// be fully managed by the code weaver and any user changes should be removed.
         /// </summary>
-        public static string Match(string path) => string.Empty;
-
+        /// <param name="attributes">One more attribute names which this instruction applies to.</param>
         public static string FullyAttributes(params string[] attributes) => string.Empty;
+
+        /// <summary>
+        /// The body content of this element must be fully managed by the code weaver and any
+        /// user changes should be removed.
+        /// </summary>
+        /// <param name="path">
+        /// Optional. Move the element's location from where it was in the generated content based
+        /// on the provided <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see>.
+        /// </param>
+        public static string FullyBody(string path = null) => string.Empty;
+
+        /// <summary>
+        /// The signature content of this element must be fully managed by the code weaver and
+        /// any user changes should be removed.
+        /// </summary>
+        /// <param name="path">
+        /// Optional. Move the element's location from where it was in the generated content based
+        /// on the provided <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see>.
+        /// </param>
+        public static string FullySignature(string path = null) => string.Empty;
+
+        /// <summary>
+        /// This element must be ignored by the code weaver.
+        /// </summary>
+        /// <param name="path">
+        /// Optional. Move the element's location from where it was in the generated content based
+        /// on the provided <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see>.
+        /// </param>
+        public static string Ignore(string path = null) => string.Empty;
+
+        /// <summary>
+        /// The specified <paramref name="attributes"/> of this element must be ignored by the code
+        /// weaver.
+        /// </summary>
+        /// <param name="attributes">One more attribute names which this instruction applies to.</param>
         public static string IgnoreAttributes(params string[] attributes) => string.Empty;
+
+        /// <summary>
+        /// The body content of this element must be ignored by the code weaver.
+        /// </summary>
+        /// <param name="path">
+        /// Optional. Move the element's location from where it was in the generated content based
+        /// on the provided <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see>.
+        /// </param>
+        public static string IgnoreBody(string path = null) => string.Empty;
+
+        /// <summary>
+        /// The signature content of this element must be ignored by the code weaver.
+        /// </summary>
+        /// <param name="path">
+        /// Optional. Move the element's location from where it was in the generated content based on the provided <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see>.
+        /// </param>
+        public static string IgnoreSignature(string path = null) => string.Empty;
+
+        /// <summary>
+        /// This element must be merged by the code weaver, any user changes to it should be ignored
+        /// while changes from the template should be applied.
+        /// </summary>
+        /// <param name="path">
+        /// Optional. Move the element's location from where it was in the generated content based
+        /// on the provided <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see>.
+        /// </param>
+        public static string Merge(string path = null) => string.Empty;
+
+        /// <summary>
+        /// The specified <paramref name="attributes"/> of this element must be merged by the code
+        /// weaver, any user changes to it should be ignored while changes from the template should
+        /// be applied.
+        /// </summary>
+        /// <param name="attributes">One more attribute names which this instruction applies to.</param>
         public static string MergeAttributes(params string[] attributes) => string.Empty;
+
+        /// <summary>
+        /// The body content of this element must be merged by the code weaver, any user changes
+        /// to it should be ignored while changes from the template should be applied.
+        /// </summary>
+        /// <param name="path">
+        /// Optional. Move the element's location from where it was in the generated content based
+        /// on the provided <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see>.
+        /// </param>
+        public static string MergeBody(string path = null) => string.Empty;
+
+        /// <summary>
+        /// The signature content of this element must be merged by the code weaver, any user
+        /// changes to it should be ignored while changes from the template should be applied.
+        /// </summary>
+        /// <param name="path">
+        /// Optional. Move the element's location from where it was in the generated content based
+        /// on the provided <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see>.
+        /// </param>
+        public static string MergeSignature(string path = null) => string.Empty;
+
+        /// <summary>
+        /// This element's location should be moved from where it was in the generated output as
+        /// specified by the provided <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path">
+        /// The original <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">path</see> of the
+        /// element in the generated content.
+        /// </param>
+        public static string MoveHere(string path) => string.Empty;
+
+        /// <summary>
+        /// Elements at the specified <paramref name="paths"/> in the generated output are skipped
+        /// over and not added to the existing file.
+        /// </summary>
+        /// <param name="paths">
+        /// The <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">paths</see> of the
+        /// elements in the generated content to skip over.
+        /// </param>
+        public static string Skip(params string[] paths) => string.Empty;
 
         public class SkipAttribute : Attribute
         {
             /// <summary>
-            /// Elements at the specified XPath-like <paramref name="paths"/> in the template are
-            /// skipped over and not added to the existing file. See the
-            /// <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">documentation</see> for
-            /// further details.
+            /// Elements at the specified <paramref name="paths"/> in the generated output are skipped
+            /// over and not added to the existing file.
             /// </summary>
+            /// <param name="paths">
+            /// The <see href="https://intentarchitect.com/redirect/bGkzQ7Pq">paths</see> of the
+            /// elements in the generated content to skip over.
+            /// </param>
             public SkipAttribute(params string[] paths) { }
         }
     }
